50ab47af13eb3c928594487468aeef8898e4c8f7
Wendy Gwo <wgwo@hotmail.com>
2015-02-17 20:38:12 -0800

    Heroku app image test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,19 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-#admin = Owner.create(username: 'admin',first_name: 'admin', last_name:'admin', password: 'password')
+admin = Owner.create(username: 'admin',first_name: 'admin', last_name:'admin', password: 'password')
 
-#owner1 = Owner.create(username: 'fredflintstone',first_name: 'Fred', last_name:'Flintstone', password: 'password')
-#dog1 =  Dog.create(dog_name: 'Dino', breed:'dinosaur', birthday:'2010-01-01',food_low_flag:false,treats_low_flag:false)
-#owner1.relationships.create(dog: dog1)
-#admin.relationships.create(dog: dog1)
+owner1 = Owner.create(username: 'fredflintstone',first_name: 'Fred', last_name:'Flintstone', password: 'password')
+dog1 =  Dog.create(dog_name: 'Dino', breed:'dinosaur', birthday:'2010-01-01',food_low_flag:false,treats_low_flag:false)
+owner1.relationships.create(dog: dog1)
+admin.relationships.create(dog: dog1)
 
-# owner2 = Owner.create(username: 'georgejetson',first_name: 'George', last_name:'Jetson', password: 'password')
-#dog2 =  Dog.create(dog_name: 'Astro', breed:'space dog', birthday:'2000-01-01',food_low_flag:false,treats_low_flag:false)
-#owner2.relationships.create(dog: dog2)
-#admin.relationships.create(dog: dog2)
+owner2 = Owner.create(username: 'georgejetson',first_name: 'George', last_name:'Jetson', password: 'password')
+dog2 =  Dog.create(dog_name: 'Astro', breed:'space dog', birthday:'2000-01-01',food_low_flag:false,treats_low_flag:false)
+owner2.relationships.create(dog: dog2)
+admin.relationships.create(dog: dog2)
 
-# owner3 = Owner.create(username: 'cliffordsowner',first_name: 'Clifford', last_name:'Owner', password: 'password')
-# dog3 =  Dog.create(dog_name: 'Clifford', breed:'big red dog', birthday:'1900-01-01',food_low_flag:false,treats_low_flag:false)
-# owner3.relationships.create(dog: dog3)
-# admin.relationships.create(dog: dog3)
+owner3 = Owner.create(username: 'cliffordsowner',first_name: 'Clifford', last_name:'Owner', password: 'password')
+dog3 =  Dog.create(dog_name: 'Clifford', breed:'big red dog', birthday:'1900-01-01',food_low_flag:false,treats_low_flag:false)
+owner3.relationships.create(dog: dog3)
+admin.relationships.create(dog: dog3)
